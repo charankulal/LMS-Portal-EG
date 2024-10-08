4b75da28931c044b511d1f9107e93ff6532ff486
charankulal <charankulal0241@gmail.com>
2024-10-08 09:52:06 +0530

    add: added few api end points

--- LMS.api/Controllers/SprintController.cs
+++ LMS.api/Controllers/SprintController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LMS.api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LMS.api.Controllers
 {
+    [ApiController]
+    [Route("api/sprints")]
     public class SprintController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDBContext _context;
+
+        public SprintController(ApplicationDBContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // create a new sprint
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateSprint([FromBody] Sprints data)
+        {
+            var sprints = new List<Sprints>();
+
+            sprints = await _context.Sprints.ToListAsync();
+            _context.Add(data);
+            await _context.SaveChangesAsync();
+
+            sprints = await _context.Sprints.ToListAsync();
+
+            return new JsonResult(sprints);
         }
     }
 }

944b0287c6e840c2fb67f049cb0bd86cf31d7428
charankulal <charankulal0241@gmail.com>
2024-10-09 23:54:34 +0530

    feature: API end points for Batches and Contents/Posts

--- LMS.api/Controllers/ContentController.cs
+++ LMS.api/Controllers/ContentController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LMS.api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LMS.api.Controllers
 {
+    [Route("api/posts")]
+    [ApiController]
     public class ContentController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDBContext _context;
+        public ContentController(ApplicationDBContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // Create a new post
+        [HttpPost("create-post")]
+        public async Task<IActionResult> CreateNewBatch([FromBody] Contents data)
+        {
+            var posts = new List<Contents>();
+
+            posts = await _context.Contents.ToListAsync();
+            _context.Add(data);
+            await _context.SaveChangesAsync();
+
+            posts = await _context.Contents.ToListAsync();
+
+            return new JsonResult(posts);
+        }
+
+        //Get Posts by the sprint Id
+        [HttpGet("view-posts/{SprintId}")]
+        public async Task<IActionResult>  GetPostsBySprintId(int SprintId)
+        {
+            var posts = new List<Contents>();
+            posts= await _context.Contents.Where(c=>c.SprintId==SprintId).ToListAsync();
+
+            return new JsonResult(posts);
         }
     }
 }
